188359be40b849e33835655e46365ab49fcb9ab5
Peter White <desourdesourde@gmail.com>
2019-04-23 11:10:22 -0700

    out and getNthRow working

--- Matrix.py
+++ Matrix.py
@@ -10,44 +10,28 @@ returns: An evaluated and solved matrix
 
 from __future__ import annotations
 
-# An element is a member of a Ring with Identity
-# This is a class with addition and multiplication and multiplicative
-# and additive identities, which is enough to support matrix
-# multiplication.
-class Elem:
-    # Multiplicative Identity (e.g. 1 for integers)
-    def mulid(self) -> Elem:
-        pass
-    # Additive Identity (e.g. 0 for integers)
-    def addid(self) ->  Elem:
-        pass
-    # Multiply two elements
-    def mul(self, a, b) -> Elem:
-        pass
-    # Add these two elements
-    def add(Elem, a, b) -> Elem:
-        pass
-    # Read an element from input
-    def read(self) -> Elem:
-        pass
-    # Print out an element
-    def out(self,x) -> Elem:
-        pass
-
-class IntCRI(Elem):
-    def addid(self): return 0
-    def mulid(self): return 1
-    def mul(self,a,b): return a*b
-    def add(self,a,b): return a+b
-
-    def read(self): int(input())
-    def out(self,x): print(x)
-
 # For now, only two-dimensional matrices are represented
 # Later we can generalize to n-dimensional matrices
 # Infinite dimensional matrices will require some more work :)
 # unless we use Haskell :)
 class Matrix():
+    # The initial representation of the matrix
+    matrix = {}
+
+    # Zero filled matrix (Additive identity for matrices)
+    def zero(self,dim) -> Matrix:
+        zeroMatrix = {'dim':dim}
+        rows = dim[0]
+        cols = dim[1]
+        for i in range(0, rows):
+            for j in range(0, cols):
+                zeroMatrix[(i,j)] = 0
+
+    # Initializer for the matrix. It can be initialized by a dictionary
+    def __init__ (self, d:dict):
+        self.matrix = d
+        return
+
     # Identity matrix (all 1s on the diagonal, zeros elsewhere)
     # This is the multiplicative identity for matrices
 
@@ -62,9 +46,9 @@ class Matrix():
     # Argument dim: A pair specifying the size of the matrix
     # A typical call looks like: id((3,3))
     def id(self, dim) -> Matrix:
-        identityMatrix = {'dim':dim}
         rows = dim[0]
         cols = dim[1]
+        identityMatrix = {'dim':dim}
         # Identity matrix must be a square matrix
         if rows != cols:
             return None
@@ -79,31 +63,22 @@ class Matrix():
                     identityMatrix[(i,j)] = 0
         return identityMatrix
 
-    # Zero filled matrix (Additive identity for matrices)
-    def zero(self,dim) -> Matrix:
-        zeroMatrix = {'dim':dim}
-        rows = dim[0]
-        cols = dim[1]
-        for i in range(0, rows):
-            for j in range(0, cols):
-                zeroMatrix[(i,j)] = 0
-
-    # Multiply two matrices
-    def mul(self, a, b) -> Matrix:
-        dima = a['dim']
+    # Multiply the matrix by another matrix
+    def mul(self, b) -> Matrix:
+        dima = self.matrix['dim']
         dimb = b['dim']
         # Number of columns in a must equal number of rows in b
         if dima[1] != dimb[0]:
             return None
-        productMatrix = {'dim':(dima[0], dimb[1])}
         rows = dima[0]
         cols = dimb[1]
+        productMatrix = {'dim':(rows, cols)}
         for i in range(0, rows):
             for j in range(0, cols):
                 productMatrix[(i,j)] = 0
                 for k in range(0, dima[1]):
-                    productMatrix[(i,j)] += a[(i,k)] * b[(k,j)]
-        return productMatrix
+                    productMatrix[(i,j)] += matrix[(i,k)] * b[(k,j)]
+        return Matrix(productMatrix)
 
     # Add two matrices
     def add(self, a, b) -> Matrix:
@@ -132,8 +107,8 @@ class Matrix():
         return powerMatrix
 
     # Access the element at a given set of indices for the matrix
-    def access(self, a, idx):
-        return a[idx]
+    def access(self, idx):
+        return matrix[idx]
 
     # Read in a matrix of the element type
     # The matrix is presented as a dictionary in the input
@@ -141,34 +116,37 @@ class Matrix():
         return eval(f.read())
 
     # Get the nth row of the matrix a, presented as a list
-    def getNthRow(self, a, n):
-        dima = a['dim']
+    def getNthRow(self, n):
+        dim = self.matrix['dim']
         rows = dim[0]
         cols = dim[1]
         if n >= rows or n < 0:
             return None
         # Get a list for the nth row
         rown = []
-        for x in a:
-            if a[0] == n:
+        for x in self.matrix:
+            # Skip the dimension element
+            if x == 'dim':
+                continue
+            if x[0] == n:
                 rown.append(x)
         return rown
 
     # Print out an element of the matrix
-    def out(self, a) -> Matrix:
-        dima = a['dim']
+    def out(self) -> Matrix:
+        print('out: ', self.matrix)
+        dim = self.matrix['dim']
         rows = dim[0]
         cols = dim[1]
         for j in range(rows):
-            rowj = getNthRow9(a,j)
+            rowj = self.getNthRow(j)
             print(rowj)
 
-testMatrix1 = {'dim': (2,2), (0,0):7, (0,1):5, (1,0):2, (1,1):4}
-testMatrix2 = {'dim': (2,2), (1,1):4, (0,1):5, (1,0):2, (0,0):7}
+testMatrix1 = Matrix({'dim': (2,2), (0,0):7, (0,1):5, (1,0):2, (1,1):4})
+testMatrix2 = Matrix({'dim': (2,2), (1,1):4, (0,1):5, (1,0):2, (0,0):7})
 
 testFile = open('test', 'r')
 
-m = Matrix()
-x = m.mul(testMatrix1, testMatrix2)
-m.out(x)
+# outMatrix1 = testMatrix1.mul(testMatrix2)
+# outMatrix1.out()
 
