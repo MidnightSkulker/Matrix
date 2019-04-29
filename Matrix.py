0279db1ec3b9aacf75b059b028273f6d0f77bf3b
Peter White <desourdesourde@gmail.com>
2019-04-29 09:03:05 -0700

    Put name and dim fields, matrix is just values now

--- Matrix.py
+++ Matrix.py
@@ -4,8 +4,8 @@
 Haskelle White
 Period 3 CSP
 
-parameters: integers, floats, rationals, any number
-returns: An evaluated and solved matrix
+parameters: See the method comments
+returns: Per method
 '''
 
 # This import supports class annotations, e.g. def mulid(self) -> Elem
@@ -15,13 +15,16 @@ class Matrix():
     # The initial representation of the matrix
     # Representation of the matrix
     # The matrix will be a dictionary of indices with values, like this
-    # {'dim':(2,2), (0,0):1, (0,1):2, (1,0):0, (1,1):3}
+    # {(0,0):1, (0,1):2, (1,0):0, (1,1):3}
+    nom = "m"
+    dimension = (0, 0)
     matrix = {}
 
     # Return the dimensions of the matrix
-    def dim(self): return self.matrix['dim']
-    def rows(self): return self.matrix['dim'][0]
-    def cols(self): return self.matrix['dim'][1]
+    def dim(self): return self.dimension
+    def rows(self): return self.dimension[0]
+    def cols(self): return self.dimension[1]
+    def naym(self): return self.nom
 
     # Zero filled matrix (Additive identity for matrices)
     def zero(self,dim) -> Matrix:
@@ -32,7 +35,9 @@ class Matrix():
         return Matrix(zeroMatrix)
 
     # Initializer for the matrix. It can be initialized by a dictionary
-    def __init__ (self, d:dict):
+    def __init__ (self, n:str, dim, d:dict):
+        self.nom = n
+        self.dimension = dim
         self.matrix = d
         return
 
@@ -46,7 +51,7 @@ class Matrix():
         # Identity matrix must be a square matrix
         if dim[0] != dim[1]:
             return None
-        identityMatrix = {'dim':dim}
+        identityMatrix = {}
         # Enter 1 in each diagonal element, and 0 elsewhere
         for i in range(0,dim[0]):
             for j in range(0,dim[1]):
@@ -56,7 +61,7 @@ class Matrix():
                 # Otherwise it is off the diagonal
                 else:
                     identityMatrix[(i,j)] = 0
-        return Matrix(identityMatrix)
+        return Matrix('id', dim, identityMatrix)
 
     # Multiply the matrix by another matrix
     def mul(self, b) -> Matrix:
@@ -67,24 +72,24 @@ class Matrix():
             return None
         rows = dima[0]
         cols = dimb[1]
-        productMatrix = {'dim':(rows, cols)}
+        productMatrix = {}
         for i in range(0, rows):
             for j in range(0, cols):
                 productMatrix[(i,j)] = 0
                 for k in range(0, dima[1]):
                     productMatrix[(i,j)] += self.matrix[(i,k)] * b.access((k,j))
-        return Matrix(productMatrix)
+        return Matrix(self.naym() + '*' + b.naym(), (rows, cols), productMatrix)
 
     # Add two matrices
     def add(self, b:Matrix) -> Matrix:
         # To add two matrices, they must have the same shape
         if self.dim() != b.dim():
             return None
-        sumMatrix = {'dim':(self.rows(), self.cols())}
+        sumMatrix = {}
         for i in range(0, self.rows()):
             for j in range(0, self.cols()):
                 sumMatrix[(i,j)] = self.access((i,j)) + b.access((i,j))
-        return Matrix(sumMatrix)
+        return Matrix(self.naym() + '+' + b.naym(), self.dim(), sumMatrix)
 
     # Subtract two matrices
     def sub(self, b:Matrix) -> Matrix:
@@ -92,11 +97,11 @@ class Matrix():
 
     # Negative of matrix
     def neg(self) -> Matrix:
-        negMatrix = {'dim':(self.rows(), self.cols())}
+        negMatrix = {}
         for i in range(0, self.rows()):
             for j in range(0, self.cols()):
                 negMatrix[(i,j)] = - self.access((i,j))
-        return Matrix(negMatrix)
+        return Matrix('-' + self.nom, self.dim(), negMatrix)
 
     # Raise a matrix a to an integer power n
     def power(self, n:int) -> Matrix:
@@ -124,7 +129,6 @@ class Matrix():
         rownidx = []
         # Get the list of keys for the nth row
         for x in self.matrix:
-            if x == 'dim': continue # Skip the dimension element
             if x[0] == n: rownidx.append(x)
         rownidx.sort()
         rown = []
@@ -133,33 +137,33 @@ class Matrix():
         return rown
 
     # Print out an element of the matrix
-    def out(self,s) -> Matrix:
+    def out(self) -> Matrix:
         # print('raw matrix', s, self.matrix)
-        print('matrix', s, 'with dimensions', self.dim())
+        print('matrix', self.nom , 'with dimensions', self.dim())
         rowz = self.rows()
         for j in range(rowz):
             print(self.getNthRow(j))
 
-testMatrix1 = Matrix({'dim': (2,2), (0,0):7, (0,1):5, (1,0):2, (1,1):4})
-testMatrix2 = Matrix({'dim': (2,2), (1,1):4, (0,1):5, (1,0):2, (0,0):7})
-testMatrix3 = Matrix({'dim': (2,2), (0,0):-2, (0,1):3, (1,0):8, (1,1):0})
+testMatrix1 = Matrix('testMatrix1', (2,2), {(0,0):7, (0,1):5, (1,0):2, (1,1):4})
+testMatrix2 = Matrix('testMatrix2', (2,2), {(1,1):4, (0,1):5, (1,0):2, (0,0):7})
+testMatrix3 = Matrix('testMatrix3', (2,2), {(0,0):-2, (0,1):3, (1,0):8, (1,1):0})
 
 testFile = open('test', 'r')
 
-testMatrix1.out('testMatrix1')
-testMatrix2.out('testMatrix2')
-testMatrix3.out('testMatrix3')
+testMatrix1.out()
+testMatrix2.out()
+testMatrix3.out()
 outMatrix1 = testMatrix1.mul(testMatrix2)
-outMatrix1.out('outMatrix1 (mul)')
+outMatrix1.out()
 outMatrix2 = testMatrix3.neg()
-outMatrix2.out('outMatrix2 (neg)')
+outMatrix2.out()
 outMatrix3 = testMatrix1.add(testMatrix3)
-outMatrix3.out('outMatrix3 (add)')
+outMatrix3.out()
 outMatrix4 = testMatrix1.sub(testMatrix3)
-outMatrix4.out('outMatrix4 (sub)')
+outMatrix4.out()
 outMatrix5 = testMatrix2.identity((2,2))
-outMatrix5.out('outMatrix5 (identity)')
+outMatrix5.out()
 outMatrix6 = outMatrix1.add(outMatrix2)
-outMatrix6.out('outMatrix6 (add)')
+outMatrix6.out()
 outMatrix7 = testMatrix1.power(3)
-outMatrix7.out('outMatrix7 (power)')
+outMatrix7.out()
